4842f8b22fe7a0d7502a0439b7598bbc3f1f71f3
akshatjoddd <akshatrane2007@gmail.com>
2026-04-20 19:37:41 +0530

    Update HeroSection.jsx
    updated navbar call

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react' 
-import useFetch from './useFetch' 
+import useFetch from './useFetch'  
+import Navbar from './Navbar'
 const apiKey = import.meta.env.VITE_API_KEY
 const HeroSection = () => { 
     const movie=useFetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&&with_genres=28`)  
@@ -11,7 +12,8 @@ const HeroSection = () => {
                 backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.7), transparent), 
                         url(https://image.tmdb.org/t/p/original${movie[randomNumber]?.backdrop_path})`,
             }}
-            > 
+            >  
+            <Navbar/>
             <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"80vh"}}>
                 <div id="HeroMovieData">
                     <h1 style={{fontSize:"60px"}}>{movie[randomNumber]?.title}</h1>
