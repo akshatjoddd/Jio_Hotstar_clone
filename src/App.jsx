8c3b7736c72e3ab6978688aee378be5a02f57bd4
akshatjoddd <akshatrane2007@gmail.com>
2026-04-20 19:45:44 +0530

    Update App.jsx
    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
-import MovieRow from "./components/MovieRow"  
-import HeroSection from "./components/HeroSection"  
-const apiKey = import.meta.env.VITE_API_KEY
-import "./App.css"
-function App(){
+import { useState } from "react";
+import MovieRow from "./components/MovieRow";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import "./App.css";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [query, setQuery] = useState("");
+
   return (
-    <div>  
-      <HeroSection/>
-      <MovieRow 
-        url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&&with_genres=28`}
-        genre="Action" 
-      /> 
-      <MovieRow 
-        url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&&with_genres=53`}
-        genre="Thriller" 
-      /> 
-      <MovieRow 
-        url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&&with_genres=10749`}
-        genre="Romance" 
-      /> 
-      <MovieRow 
-        url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&&with_genres=99`}
-        genre="Documentary" 
-      />  
-      
+    <div className="app">
+      <Navbar setQuery={setQuery} />
+
+      {query ? (
+        <Search query={query} />
+      ) : (
+        <div>
+          <HeroSection />
+
+          <MovieRow
+            url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28`}
+            genre="Action"
+          />
+
+          <MovieRow
+            url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=53`}
+            genre="Thriller"
+          />
+
+          <MovieRow
+            url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=10749`}
+            genre="Romance"
+          />
+
+          <MovieRow
+            url={`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=99`}
+            genre="Documentary"
+          />
+        </div>
+      )}
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
